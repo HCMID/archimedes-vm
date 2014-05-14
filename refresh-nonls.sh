1f42b6d80214ead5aab39e2fabd4f490a267bbac
Neel Smith <dnsmith.neel@gmail.com>
2014-05-14 11:00:05 -0400

    Add script with no new lines.

---
+++ refresh-nonls.sh
@@ -0,0 +1,81 @@
+#/bin/bash
+#
+# User fuser to kill any running services so
+# we can invoke this script any time without
+# rebooting server.
+#
+
+echo Starting the archimedes-hc servlet
+GIT=`which git`
+GRADLE=`which gradle`
+echo Will use git from $GIT
+echo Will use gradle from $GRADLE
+# What we need:
+# 1. the archimedes-hc archive.
+# 2. citemgr
+# 3. citeservlet
+
+echo ""
+echo "---------------------------------"
+echo "1. Check for current repositories"
+echo "---------------------------------"
+# archive:
+
+echo "Installing archimedes-hc archive"
+cd /vagrant
+echo  Running  /usr/bin/git clone https://github.com/HCMID/archimedes-vm.git
+/usr/bin/git clone https://github.com/HCMID/archimedes-vm.git
+# citemgr
+echo "Installing CITE archive manager."
+cd /vagrant
+echo  Running  /usr/bin/git clone https://github.com/cite-architecture/citemgr.git
+/usr/bin/git clone https://github.com/cite-architecture/citemgr.git
+
+echo "Installing CITE servlet."
+cd /vagrant
+echo  Running  /usr/bin/git clone https://github.com/cite-architecture/citeservlet.git
+/usr/bin/git clone https://github.com/cite-architecture/citeservlet.git
+
+
+# With everything up to date, then:
+echo ""
+echo "--------------------------------------"
+echo 2. All files up date.  Now building TTL.
+echo "--------------------------------------"
+# 1. build TTL
+cd /vagrant/citemgr
+echo Building project RDF graph.
+echo This can take a couple of minutes.
+echo ""
+/usr/bin/gradle clean && /usr/bin/gradle -Pconf=/vagrant/sparql/citemgr-conf.gradle ttl
+/bin/cp /vagrant/citemgr/build/ttl/all.ttl /vagrant/sparql
+echo TTL build.  Now loading into fuseki.
+
+
+
+echo ""
+echo "------------------------------------------------------------"
+echo 3. Graph built. Now loading data and starting SPARQL endpoint.
+echo "------------------------------------------------------------"
+
+
+echo "Loading new data into RDF server."
+/bin/mkdir /vagrant/sparql/tdbs
+/vagrant/jena/bin/tdbloader2 -loc /vagrant/sparql/tdbs /vagrant/sparql/all.ttl
+
+echo "Starting fuseki"
+cd /vagrant/fuseki
+./fuseki-server --port=3030 --config=/vagrant/sparql/fuseki-conf.ttl &
+
+
+
+echo ""
+echo "--------------------------------"
+echo 4. Now starting servlet container
+echo "--------------------------------"
+
+# 3. start servlet
+cd /vagrant/citeservlet
+
+/usr/bin/gradle clean && /usr/bin/gradle   -Pconf=/vagrant/archimedes-hc/confs/localconf.gradle   -Plinks=/vagrant/archimedes-hc/confs/locallinks.gradle   -Pcustom=/vagrant/archimedes-hc/servlet/ jettyRunWar  &
+
